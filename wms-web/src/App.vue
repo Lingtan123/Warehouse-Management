63685134e405d7265e98379151064b83eb2ae2f6
Lingtan123 <2338636515@qq.com>
2026-07-01 20:22:06 +0800

    web

---
+++ wms-web/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HomeIndex></HomeIndex>
+  </div>
+</template>
+
+<script>
+
+import HomeIndex from "@/components/Index.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HomeIndex
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100vh;
+}
+</style>
